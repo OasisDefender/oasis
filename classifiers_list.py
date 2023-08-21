6295117d3ff4c273c3567e68c0da4b4dcbc8a58c
Sergei Shu <gitmail@ctxctx.anonaddy.com>
2023-08-21 17:05:50 +0300

    add some data for test frontend part of <flex classifier>

--- classifiers_list.py
+++ classifiers_list.py
@@ -2,6 +2,28 @@ class classifier:
     def __init__(self):
         self.items = []
         self.selected = []
+        self.items.append({"name": "Cloud name", "description": "Name of the cloud", "class_name": "Cloud",
+                          "field": "name", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Cloud type", "description": "Type of the cloud", "class_name": "Cloud",
+                          "field": "cloud_type", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Region", "description": "Region", "class_name": "Cloud",
+                          "field": "aws_region", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "VPC Name", "description": "VPC Name", "class_name": "VPC",
+                          "field": "name", "fn": None, "node_type": "VPC", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "VPC Network", "description": "VPC network", "class_name": "VPC",
+                          "field": "network", "fn": None, "node_type": "VPC", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Subnet name", "description": "Subnet name", "class_name": "Subnet",
+                          "field": "name", "fn": None, "node_type": "Subnet", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Subnet ARN", "description": "Subnet ARN", "class_name": "Subnet",
+                          "field": "arn", "fn": None, "node_type": "Subnet", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Subnet Network", "description": "Subnet network", "class_name": "Subnet",
+                          "field": "network", "fn": None, "node_type": "Subnet", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "VM OS", "description": "Host Operating system", "class_name": "VM",
+                          "field": "os", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "VM State", "description": "Host current state", "class_name": "VM",
+                          "field": "state", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
+        self.items.append({"name": "Security group", "description": "Securityh group name", "class_name": "RuleGroup",
+                          "field": "name", "fn": None, "node_type": "Cloud", "node_icon": "IconInfoCircle"})
 
     def add(self, name, description, class_name, field, fn=None, node_type="Cloud", node_icon="IconInfoCircle"):
         self.item = {}
