395316250ebca6623485955594907428c97bbb12
aleks_eeca <aleks7272@live.com>
2023-10-13 16:23:46 +0300

    lambda handlers and backend implimintation

--- backend/backend.py
+++ backend/backend.py
@@ -24,272 +24,288 @@ class Backend(CTX):
     def __init__(self):
         pass
 
-
     def get_clouds(self):
-        db = DB(self.get_ctx())
-        clouds = db.get_clouds()
-        return [c.to_dict() for c in clouds]
-
+        status:int = 200
+        body       = None
+        try:
+            db = DB(self.get_ctx())
+            clouds = db.get_clouds()
+            body = [c.to_dict() for c in clouds]
+        except:
+            status = 500
+            body = {'message': "Database error"}
+        return status, body
 
     def cloud_sync(self, id: int):
         fw = None
         context = DB(self.get_ctx())
-        retmsg = ""
-        retval = 200
+        body = None
+        status = 200
         cloud: Cloud = None
-
-        for cloud in context.get_clouds():
-            if cloud.id == id:
-                if cloud.cloud_type == 'AWS':
-                    fw = FW_AWS()
-                    break
-                if cloud.cloud_type == 'AZURE':
-                    fw = FW_Azure()
-                    break
-                retmsg = f"Cloud Type: '{cloud.cloud_type}' - not supported!"
-                retval = 500
-        if fw != None:
-            fw.save_ctx(self.get_ctx())
-            context.sync_cloud(cloud.id)
-            fw.connect(cloud.id)
-            fw.get_topology(cloud.id)
-        return retmsg, retval
-
+        try:
+            for cloud in context.get_clouds():
+                if cloud.id == id:
+                    if cloud.cloud_type == 'AWS':
+                        fw = FW_AWS()
+                        break
+                    if cloud.cloud_type == 'AZURE':
+                        fw = FW_Azure()
+                        break
+                    status = 500
+                    body = {'message': f"Cloud Type: '{cloud.cloud_type}' - not supported!"}
+            if fw != None:
+                fw.save_ctx(self.get_ctx())
+                context.sync_cloud(cloud.id)
+                fw.connect(cloud.id)
+                fw.get_topology(cloud.id)
+        except:
+            status = 500
+            body = {'message': "Cloud sync error"}
+        return status, body
 
     def cloud_delete(self, id: int):
-        db = DB(self.user_id)
-        db.delete_cloud(id)
-        return "", 200
+        body = None
+        status = 200
+        try:
+            db = DB(self.user_id)
+            db.delete_cloud(id)
+        except:
+            status = 500
+            body = {'message': "Database error"}
+        return status, body
 
-
-    def get_header_info(self):
-        db = DB(self.get_ctx())
-        clouds = db.get_clouds()
-        map = CloudMap()
-        map.save_ctx(self.get_ctx())
-        map.get()
-        vpcs = map.vpcs
-
-        sgs = get_all_rule_groups(self.get_ctx())
-        rules = get_all_rules(self.get_ctx())
-        nodes = Nodes(db.get_all_nodes_info())
-        s = []
-        for v in vpcs:
-            s = s + v.subnets
-        subnets = [*set(s)]
-
-        l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
-        l.save_ctx(self.get_ctx())
-        l.make_links()
-        l.analyze_links()
-        res = l.get_max_severity()
-
-        return jsonify({"maxSeverity": res})
-
-    def cloud_add(self, reqCloud: str):
-        context = DB(self.get_ctx())
-        # Sanitize
-        reqCloud["cloud_type"] = reqCloud["cloud_type"].upper()
-        cloud_type = reqCloud['cloud_type']
-        if cloud_type == "AWS":
-            cloud = Cloud(id=-1,
-                        name=reqCloud['name'],
-                        cloud_type=reqCloud['cloud_type'],
-                        aws_region=reqCloud['aws_region'],
-                        aws_key=reqCloud['aws_key'],
-                        aws_secret_key=reqCloud['aws_secret_key'],
-                        azure_subscription_id=None,
-                        azure_tenant_id=None,
-                        azure_client_id=None,
-                        azure_client_secret=None)
-        elif cloud_type == "AZURE":
-            cloud = Cloud(id=-1,
-                        name=reqCloud['name'],
-                        cloud_type=reqCloud['cloud_type'],
-                        aws_region=None,
-                        aws_key=None,
-                        aws_secret_key=None,
-                        azure_subscription_id=reqCloud['azure_subscription_id'],
-                        azure_tenant_id=reqCloud['azure_tenant_id'],
-                        azure_client_id=reqCloud['azure_client_id'],
-                        azure_client_secret=reqCloud['azure_client_secret'])
-        else:
-            return "Unsupported cloud type", 500
-
-        # Add to DB
-        context.add_cloud(cloud)
-        if cloud.id <= 0:
-            return "Can't insert new cloud! Already exists?", 500
-
-        # Test cloud
-        save_cloud_id = cloud.id
-        fw = None
-        if cloud.cloud_type == 'AWS':
-            fw = FW_AWS()
-        if cloud.cloud_type == 'AZURE':
-            fw = FW_Azure()
-        if fw != None:
-            fw.save_ctx(self.get_ctx())
-            if cloud.id == fw.connect(cloud.id):
-                fw.get_topology(cloud.id)
+    def cloud_add(self, cloud):
+        body = None
+        status = 200
+        try:
+            context = DB(self.get_ctx())
+            # Sanitize
+            cloud["cloud_type"] = cloud["cloud_type"].upper()
+            cloud_type = cloud['cloud_type']
+            if cloud_type == "AWS":
+                cloud = Cloud(id=-1,
+                            name=cloud['name'],
+                            cloud_type=cloud['cloud_type'],
+                            aws_region=cloud['aws_region'],
+                            aws_key=cloud['aws_key'],
+                            aws_secret_key=cloud['aws_secret_key'],
+                            azure_subscription_id=None,
+                            azure_tenant_id=None,
+                            azure_client_id=None,
+                            azure_client_secret=None)
+            elif cloud_type == "AZURE":
+                cloud = Cloud(id=-1,
+                            name=cloud['name'],
+                            cloud_type=cloud['cloud_type'],
+                            aws_region=None,
+                            aws_key=None,
+                            aws_secret_key=None,
+                            azure_subscription_id=cloud['azure_subscription_id'],
+                            azure_tenant_id=cloud['azure_tenant_id'],
+                            azure_client_id=cloud['azure_client_id'],
+                            azure_client_secret=cloud['azure_client_secret'])
             else:
-                context.delete_cloud(save_cloud_id)
-                return f"Can't connect to cloud: '{cloud.name}' ({cloud.cloud_type}). Bad credentials or permissions?", 500
-
-        return cloud.to_dict(), 200
-
+                status = 500
+                body = {'message': "Unsupported cloud type"}
+                return status, body
+            # Add to DB
+            context.add_cloud(cloud)
+            if cloud.id <= 0:
+                status = 500
+                body = {'message': "Can't insert new cloud! Already exists?"}
+                return status, body
+            # Test cloud (sync)
+            save_cloud_id = cloud.id
+            fw = None
+            if cloud.cloud_type == 'AWS':
+                fw = FW_AWS()
+            if cloud.cloud_type == 'AZURE':
+                fw = FW_Azure()
+            if fw != None:
+                fw.save_ctx(self.get_ctx())
+                if cloud.id == fw.connect(cloud.id):
+                    fw.get_topology(cloud.id)
+                    body = cloud.to_dict()
+                else:
+                    context.delete_cloud(save_cloud_id)
+                    status = 500
+                    body = {'message': f"Can't connect to cloud: '{cloud.name}' ({cloud.cloud_type}). Bad credentials or permissions?"}
+                    return status, body
+        except:
+            status = 500
+            body = {'message': "Cloud add error"}
+        return status, body
 
     def cloud_map(self):
-        map = CloudMap()
-        map.save_ctx(self.get_ctx())
-        map.get()
-        vpcs = cloud_map_encoder(map)
-
-        inodes = InternetNodes()
-        inodes.save_ctx(self.get_ctx())
-        inodes.get()
-        internetNodes = internet_nodes_encoder(inodes)
-
-        result = {
-            "vpcs": vpcs,
-            "inodes": internetNodes
-        }
-
-        return json.dumps(result, ensure_ascii=False), 200
-
+        body = None
+        status = 200
+        try:
+            map = CloudMap()
+            map.save_ctx(self.get_ctx())
+            map.get()
+            vpcs = cloud_map_encoder(map)
+            inodes = InternetNodes()
+            inodes.save_ctx(self.get_ctx())
+            inodes.get()
+            internetNodes = internet_nodes_encoder(inodes)
+            body = {
+                "vpcs": vpcs,
+                "inodes": internetNodes
+            }
+        except:
+            status = 500
+            body = {'message': "Get cloud-map error"}
+        return status, body
 
     def get_vm_links(self, vm_id: int):
-        l = Links(vm_id)
-        l.save_ctx(self.get_ctx())
-        l.get()
-        lines = json.dumps(l, default=link_encoder, ensure_ascii=False)
-        r = VM_Rules(vm_id)
-        r.save_ctx(self.get_ctx())
-        r.get()
-        rules = json.dumps(r.to_dict())
-        reply = f"{{\"links\": {lines}, \"rules\": {rules}}}"
-        return reply
-
+        body = None
+        status = 200
+        try:
+            l = Links(vm_id)
+            l.save_ctx(self.get_ctx())
+            l.get()
+            r = VM_Rules(vm_id)
+            r.save_ctx(self.get_ctx())
+            r.get()
+            body = {
+                "links": l.to_dict(),
+                "rules": r.to_dict()
+            }
+        except:
+            status = 500
+            body = {'message': "Get VM links error"}
+        return status, body
 
     def storages_list(self):
         clouds: list[S3_Cloud] = []
-        db = DB(self.get_ctx())
-        for row in db.get_clouds_short():
-            c = S3_Cloud(id=row[0], name=row[1], type=row[2])
-            c.save_ctx(self.get_ctx())
-            c.get_buckets()
-            clouds.append(c)
-        retval = {
-            'childrenLayout': "column",
-            'children': [c.to_typescript_values() for c in clouds]
-        }
-        return jsonify(retval)
-
+        body = None
+        status = 200
+        try:
+            db = DB(self.get_ctx())
+            for row in db.get_clouds_short():
+                c = S3_Cloud(id=row[0], name=row[1], type=row[2])
+                c.save_ctx(self.get_ctx())
+                c.get_buckets()
+                clouds.append(c)
+            body = {
+                'childrenLayout': "column",
+                'children': [c.to_typescript_values() for c in clouds]
+            }
+        except:
+            status = 500
+            body = {'message': "Get storages error"}
+        return status, body
 
     def classifiers_list(self):
-        # here we need to load available classifiers
-        c = classifier()
-        return jsonify(c.get_classifiers())
-
-
-    def classification_build(self, sel):
-        # load cloud data from DB
-        context = DB(self.get_ctx())
-        clouds = context.get_clouds()
-        map = CloudMap()
-        map.get()
-        vpcs = map.vpcs
-        sgs = get_all_rule_groups()
-        rules = get_all_rules()
-        nodes = Nodes(context.get_all_nodes_info())
-        s = []
-        for v in vpcs:
-            s = s + v.subnets
-        subnets = [*set(s)]
-
-        # build classification
-        c = classifier()
-        c.set_selected(sel)
-        sas = attr_set(c)
-
-        sas.add_vm_info("Name", "note")
-        sas.add_vm_info("<br/>Priv DNS", "privdn")
-        sas.add_vm_info("<br/>Pub DNS", "pubdn")
-        sas.add_vm_info("<br/>Pub IP", "pubip")
-
-        vms = split_vms(clouds, vpcs, subnets, nodes.nodes, sgs, rules, sas)
-        t = vms.build_vms_tree(sas)
-        res = t.dump_tree()
-
-        # return it to frontend
-        return jsonify(res)
-
+        body = None
+        status = 200
+        try:
+            # here we need to load available classifiers
+            c = classifier()
+            body = c.get_classifiers()
+        except:
+            status = 500
+            body = {'message': "Get classifiers error"}
+        return status, body
 
     def classification_build2(self, sel):
-        # load cloud data from DB
-        context = DB(self.get_ctx())
-        clouds = context.get_clouds()
-        map = CloudMap()
-        map.save_ctx(self.get_ctx())
-        map.get()
-        vpcs = map.vpcs
-        sgs = get_all_rule_groups(self.get_ctx())
-        rules = get_all_rules(self.get_ctx())
-        nodes = Nodes(context.get_all_nodes_info())
-        s = []
-        for v in vpcs:
-            s = s + v.subnets
-        subnets = [*set(s)]
-
-        # build classification
-        c = classifier()
-        c.set_selected(sel)
-        sas = attr_set(c)
-
-        if DEMO_MODE:
-            sas.add_vm_info("Name", "note")
-            sas.add_vm_info("<br/>Priv DNS", "privdn")
-            sas.add_vm_info("<br/>Priv IP", "privip")
-            sas.add_vm_info("<br/>Pub DNS", "hide_pubdn")
-            sas.add_vm_info("<br/>Pub IP", "hide_pubip")
-        else:
-            sas.add_vm_info("Name", "note")
-            sas.add_vm_info("<br/>Priv DNS", "privdn")
-            sas.add_vm_info("<br/>Pub DNS", "pubdn")
-            sas.add_vm_info("<br/>Pub IP", "pubip")
-
-        vms = split_vms(clouds, vpcs, subnets, nodes.nodes, sgs, rules, sas)
-        t = vms.build_vms_tree(sas)
-        l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
-        l.make_links()
-        scheme = t.dump_tree(l.ext_things)
-        idl = t.get_idlist_by_node()
-        links = l.dump_links(idl)
-        res = {"scheme": scheme, "lines": {"items": links}}
-
+        body = None
+        status = 200
+        try:
+            # load cloud data from DB
+            context = DB(self.get_ctx())
+            clouds = context.get_clouds()
+            map = CloudMap()
+            map.save_ctx(self.get_ctx())
+            map.get()
+            vpcs = map.vpcs
+            sgs = get_all_rule_groups(self.get_ctx())
+            rules = get_all_rules(self.get_ctx())
+            nodes = Nodes(context.get_all_nodes_info())
+            s = []
+            for v in vpcs:
+                s = s + v.subnets
+            subnets = [*set(s)]
+            # build classification
+            c = classifier()
+            c.set_selected(sel)
+            sas = attr_set(c)
+            if DEMO_MODE:
+                sas.add_vm_info("Name", "note")
+                sas.add_vm_info("<br/>Priv DNS", "privdn")
+                sas.add_vm_info("<br/>Priv IP", "privip")
+                sas.add_vm_info("<br/>Pub DNS", "hide_pubdn")
+                sas.add_vm_info("<br/>Pub IP", "hide_pubip")
+            else:
+                sas.add_vm_info("Name", "note")
+                sas.add_vm_info("<br/>Priv DNS", "privdn")
+                sas.add_vm_info("<br/>Pub DNS", "pubdn")
+                sas.add_vm_info("<br/>Pub IP", "pubip")
+            vms = split_vms(clouds, vpcs, subnets, nodes.nodes, sgs, rules, sas)
+            t = vms.build_vms_tree(sas)
+            l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
+            l.make_links()
+            scheme = t.dump_tree(l.ext_things)
+            idl = t.get_idlist_by_node()
+            links = l.dump_links(idl)
+            body = {"scheme": scheme, "lines": {"items": links}}
+        except:
+            status = 500
+            body = {'message': "Classification build error"}
         # return it to frontend
-        return jsonify(res)
-
+        return status, body
 
     def analyze_links(self):
-        context = DB(self.get_ctx())
-        clouds = context.get_clouds()
-        map = CloudMap()
-        map.save_ctx(self.get_ctx())
-        map.get()
-        vpcs = map.vpcs
+        body = None
+        status = 200
+        try:
+            context = DB(self.get_ctx())
+            clouds = context.get_clouds()
+            map = CloudMap()
+            map.save_ctx(self.get_ctx())
+            map.get()
+            vpcs = map.vpcs
+            sgs = get_all_rule_groups(self.get_ctx())
+            rules = get_all_rules(self.get_ctx())
+            nodes = Nodes(context.get_all_nodes_info())
+            s = []
+            for v in vpcs:
+                s = s + v.subnets
+            subnets = [*set(s)]
+            l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
+            l.save_ctx(self.get_ctx())
+            l.make_links()
+            l.analyze_links()
+            body = l.dump_analize_rezults()
+        except:
+            status = 500
+            body = {'message': "Security analysis error"}
+        # return it to frontend
+        return status, body
 
-        sgs = get_all_rule_groups(self.get_ctx())
-        rules = get_all_rules(self.get_ctx())
-        nodes = Nodes(context.get_all_nodes_info())
-        s = []
-        for v in vpcs:
-            s = s + v.subnets
-        subnets = [*set(s)]
-
-        l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
-        l.save_ctx(self.get_ctx())
-        l.make_links()
-        l.analyze_links()
-        ar = l.dump_analize_rezults()
-        return jsonify(ar)
+    def get_header_info(self):
+        body = None
+        status = 200
+        try:
+            db = DB(self.get_ctx())
+            clouds = db.get_clouds()
+            map = CloudMap()
+            map.save_ctx(self.get_ctx())
+            map.get()
+            vpcs = map.vpcs
+            sgs = get_all_rule_groups(self.get_ctx())
+            rules = get_all_rules(self.get_ctx())
+            nodes = Nodes(db.get_all_nodes_info())
+            s = []
+            for v in vpcs:
+                s = s + v.subnets
+            subnets = [*set(s)]
+            l = links_by_rules(clouds, nodes.nodes, subnets, sgs, rules)
+            l.save_ctx(self.get_ctx())
+            l.make_links()
+            l.analyze_links()
+            body = {"maxSeverity": l.get_max_severity()}
+        except:
+            status = 500
+            body = {'message': "Get header error"}
+        return status, body
