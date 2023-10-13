60b01b3ee54a55ce705cb08a89fe40358d0a357f
a7272leks <132675055+a7272leks@users.noreply.github.com>
2023-10-13 16:25:16 +0300

    lambda handlers and backend implimintation (#83)

--- backend/app.py
+++ backend/app.py
@@ -1,82 +1,84 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_cors import CORS
+import json
 
 from backend import Backend
-
-import json
 
 
 app = Flask(__name__)
 CORS(app)
 
+def create_json_response(statusCode: int, jsonBody: str):
+    result = {
+        'statusCode': statusCode,
+        'headers': {
+        },
+        'body': None
+    }
+    if jsonBody:
+        result['headers']['Content-Type'] = 'application/json'
+        result['body'] = jsonBody
+    return result
+
 
 # API
-
 
 @app.route('/api/clouds', methods=['GET'])
 def api_clouds_get():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.get_clouds()
-
+    status, body = ctx.get_clouds()
+    return json.dumps(body), status
 
 @app.route('/api/cloud/<int:id>/sync', methods=['POST'])
 def api_cloud_sync(id: int):
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.cloud_sync(id)
-
+    status, body = ctx.cloud_sync(id)
+    return json.dumps(body), status
 
 @app.route('/api/cloud/<int:id>', methods=['DELETE'])
 def api_cloud_delete(id: int):
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.cloud_delete(id)
-
+    status, body = ctx.cloud_delete(id)
+    return json.dumps(body), status
 
 @app.route('/api/cloud', methods=['POST'])
 def api_cloud_add():
     reqCloud = request.get_json()
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.cloud_add(reqCloud)
-
+    status, body = ctx.cloud_add(reqCloud)
+    return json.dumps(body), status
 
 @app.route('/api/map', methods=['GET'])
 def api_cloud_map():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.cloud_map()
-
+    status, body = ctx.cloud_map()
+    return json.dumps(body), status
 
 @app.route('/api/vm/<int:vm_id>/links')
 def api_get_vm_links(vm_id: int):
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.get_vm_links(vm_id)
-
+    status, body = ctx.get_vm_links(vm_id)
+    return json.dumps(body), status
 
 @app.route('/api/storages', methods=['GET'])
 def api_storages_list():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.storages_list()
-
+    status, body = ctx.storages_list()
+    return json.dumps(body), status
 
 @app.route('/api/classifiers', methods=['GET'])
 def api_classifiers_list():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.classifiers_list()
-
-
-@app.route('/api/classification', methods=['POST'])
-def api_classification_build():
-    # get data from frontend
-    sel = request.get_json()
-    ctx = Backend()
-    ctx.save_ctx('db')
-    return ctx.classification_build(sel)
+    status, body = ctx.classifiers_list()
+    return json.dumps(body), status
 
 
 @app.route('/api/classification2', methods=['POST'])
@@ -85,22 +87,23 @@ def api_classification_build2():
     sel = request.get_json()
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.classification_build2(sel)
-
+    status, body = ctx.classification_build2(sel)
+    return json.dumps(body), status
 
 @app.route('/api/analyzation', methods=['GET'])
 def api_analyze_links():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.analyze_links()
+    status, body = ctx.analyze_links()
+    return json.dumps(body), status
 
 
 @app.route('/api/header-info', methods=['GET'])
 def api_get_header_info():
     ctx = Backend()
     ctx.save_ctx('db')
-    return ctx.get_header_info()
-
+    status, body = ctx.get_header_info()
+    return json.dumps(body), status
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
