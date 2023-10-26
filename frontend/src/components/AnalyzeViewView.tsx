39c1c74c5453dd79660fc8acffaf033dd1ae8fba
harmonyblend <ooo@ooo.oo>
2023-10-26 11:55:22 +0300

    Fix VM view on Security View

--- frontend/src/components/AnalyzeViewView.tsx
+++ frontend/src/components/AnalyzeViewView.tsx
@@ -34,14 +34,14 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
             return {
                 item: {
                     style: {
-                        margin: "2rem",                        
-                    },                    
+                        margin: "2rem",
+                    },
                 },
                 header: {
                     icon: "IconCloud",
                     style: {
-                        borderBottom: bg ? `10px solid ${bg}` : undefined
-                    }                  
+                        borderBottom: bg ? `10px solid ${bg}` : undefined,
+                    },
                 },
                 headerSelected: {
                     textColor: isDark ? theme.colors.blue[6] : "blue",
@@ -49,7 +49,7 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
                 layout: {
                     childrenContainerStyle: {
                         border: "1px solid gray",
-                        padding: "1rem",                    
+                        padding: "1rem",
                     },
                 },
                 layoutSelected: {
@@ -66,14 +66,14 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
             return {
                 item: {
                     style: {
-                        margin: "0.5rem"                        
+                        margin: "0.5rem",
                     },
                 },
                 header: {
                     icon: "IconGridDots",
                     style: {
-                        borderBottom: bg ? `10px solid ${bg}` : undefined
-                    }
+                        borderBottom: bg ? `10px solid ${bg}` : undefined,
+                    },
                 },
                 headerSelected: {
                     textColor: isDark ? theme.colors.blue[6] : "blue",
@@ -103,8 +103,9 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
                         border: "2px solid gray",
                         padding: "0.2rem",
                         background: isDark
-                                ? theme.colors.dark[6]
-                                : theme.colors.blue[3],
+                            ? theme.colors.dark[6]
+                            : theme.colors.blue[3],
+                        borderBottom: bg ? `10px solid ${bg}` : undefined,
                     },
                 },
                 itemSelected: {
@@ -122,18 +123,18 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
         };
 
         return {
-            "Cloud0": getCloudStyle(severityToColor(0)),
-            "Cloud1": getCloudStyle(severityToColor(1)),
-            "Cloud2": getCloudStyle(severityToColor(2)),
-            "Cloud3": getCloudStyle(severityToColor(3)),
-            "Subnet0": getSubnetStyle(severityToColor(0)),
-            "Subnet1": getSubnetStyle(severityToColor(1)),
-            "Subnet2": getSubnetStyle(severityToColor(2)),
-            "Subnet3": getSubnetStyle(severityToColor(3)),
-            "VM0": getVMStyle(severityToColor(0)),
-            "VM1": getVMStyle(severityToColor(1)),
-            "VM2": getVMStyle(severityToColor(2)),
-            "VM3": getVMStyle(severityToColor(3))
+            Cloud0: getCloudStyle(severityToColor(0)),
+            Cloud1: getCloudStyle(severityToColor(1)),
+            Cloud2: getCloudStyle(severityToColor(2)),
+            Cloud3: getCloudStyle(severityToColor(3)),
+            Subnet0: getSubnetStyle(severityToColor(0)),
+            Subnet1: getSubnetStyle(severityToColor(1)),
+            Subnet2: getSubnetStyle(severityToColor(2)),
+            Subnet3: getSubnetStyle(severityToColor(3)),
+            VM0: getVMStyle(severityToColor(0)),
+            VM1: getVMStyle(severityToColor(1)),
+            VM2: getVMStyle(severityToColor(2)),
+            VM3: getVMStyle(severityToColor(3)),
         };
     }, [isDark]);
 
@@ -142,16 +143,16 @@ const AnalyzeViewView: React.FC<PolicyMapViewProps> = ({
             return {
                 line: {
                     stroke: color,
-                    strokeOpacity: 0.6
-                }
-            }
+                    strokeOpacity: 0.6,
+                },
+            };
         };
 
         return {
-            "line0": getLineStyle(severityToColor(0)),
-            "line1": getLineStyle(severityToColor(1)),            
-            "line2": getLineStyle(severityToColor(2)),
-            "line3": getLineStyle(severityToColor(3)),
+            line0: getLineStyle(severityToColor(0)),
+            line1: getLineStyle(severityToColor(1)),
+            line2: getLineStyle(severityToColor(2)),
+            line3: getLineStyle(severityToColor(3)),
         };
     }, [isDark]);
 
