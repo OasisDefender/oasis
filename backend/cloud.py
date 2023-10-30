f267a4c158e71afe5e459c4a548e674339bdd66e
aleks_eeca <aleks7272@live.com>
2023-10-30 16:04:56 +0300

    for frontend developer only

--- backend/cloud.py
+++ backend/cloud.py
@@ -10,7 +10,10 @@ class Cloud:
                  azure_subscription_id: str,
                  azure_tenant_id: str,
                  azure_client_id: str,
-                 azure_client_secret: str):
+                 azure_client_secret: str,
+                 sync_state:int = 0,
+                 sync_start:str = '',
+                 sync_stop:str  = ''):
         self.id = id
         self.name = name
         self.cloud_type = cloud_type.upper()
@@ -21,6 +24,10 @@ class Cloud:
         self.azure_tenant_id = azure_tenant_id
         self.azure_client_id = azure_client_id
         self.azure_client_secret = azure_client_secret
+        self.sync_state = sync_state
+        self.sync_start = sync_start
+        self.sync_stop  = sync_stop
+
         
     def to_dict(self) -> dict:
         return { 
@@ -31,6 +38,9 @@ class Cloud:
               'aws_key': self.aws_key,               
               'azure_tenant_id': self.azure_tenant_id,
               'azure_client_id': self.azure_client_id,
-              'azure_subscription_id': self.azure_subscription_id
+              'azure_subscription_id': self.azure_subscription_id,
+              'sync_state': self.sync_state,
+              'sync_start': self.sync_start,
+              'sync_stop': self.sync_stop,
             }
 
